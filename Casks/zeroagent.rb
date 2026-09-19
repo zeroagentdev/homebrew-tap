1201447cf58e3b6a1e7a7b139ea9d0b4266f08bc
zeroagent-release <actions@github.com>
2026-09-19 12:27:00 -0400

    Bump zeroagent to 0.7.7

--- Casks/zeroagent.rb
+++ Casks/zeroagent.rb
@@ -1,9 +1,9 @@
 cask "zeroagent" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.7.6"
-  sha256 arm:   "e74365fb0dad79e20bb7112456ff36b1a139ef183b2f0c631f3ccc982938420d",
-         intel: "5e6a1fe1bbb6ddad39ec1354ab3cd9fa66f25e09808c6e1da2b6cb21b0863516"
+  version "0.7.7"
+  sha256 arm:   "45db3e619be639b81b270345fbc984e778c92286fb43e40765e52c5ede6d52b0",
+         intel: "45ae3fb63d4ff8a8e2bfba9cb1afc9b73faafc41bac3ee6132e8f2b29ea6013b"
 
   url "https://releases.zeroagenthq.com/ZeroAgent-#{version}-#{arch}.dmg"
   name "ZeroAgent"
